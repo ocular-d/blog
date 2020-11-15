4079df479c30c94caf14628155b652f8a3614309
sven <sven@so36.net>
2020-11-15 15:45:38 +0100

    chore(init): add first files

---
+++ .vuepress/config.js
@@ -0,0 +1,50 @@
+// .vuepress/config.js
+module.exports = {
+  title: 'ocular-d Blog', // Title for the site. This will be displayed in the navbar.
+  theme: '@vuepress/theme-blog',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+  themeConfig: {
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/ocular-d',
+        }
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © ocular-d'
+        },
+      ],
+    },
+    feed: {
+      canonical_base: 'https://blog.ocular-d.tech/'
+    },
+    sitemap: {
+      hostname: 'https://blog.ocular-d.tech'
+    },
+    smoothScroll: true,
+    summary: false,
+    searchPlaceholder: 'Search...',
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/ocular-d',
+      },
+    ]
+  }
+}
